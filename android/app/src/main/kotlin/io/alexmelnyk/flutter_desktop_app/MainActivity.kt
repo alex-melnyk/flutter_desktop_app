651cdae97f5d40c40d2f334473141c90c58812d7
Alex Melnyk <ask.melnyk@gmail.com>
2021-04-09 21:20:33 +0300

    Initial project created.

---
+++ android/app/src/main/kotlin/io/alexmelnyk/flutter_desktop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.alexmelnyk.flutter_desktop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_desktop_app/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
